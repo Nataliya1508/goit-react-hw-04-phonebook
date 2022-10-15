58bdf9f6688be24649da021d550d2a8eba977e11
Nataly <97055353+Nataliya1508@users.noreply.github.com>
2022-10-15 13:40:15 +0300

    fix useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,13 @@ import { Filter } from './Filter/Filter';
 
 
 export default function App() {
-  const [contacts, setContacts] = useState([
+  const PhoneContacts = [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ])
+    ]
+  const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem('contacts')) || PhoneContacts);
   
   const [filter, setFilter] = useState('');
   
